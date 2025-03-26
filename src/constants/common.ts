11cf13791193bf815d3b8c1f8b03880d0187b632
trieunv <trieuhl@gmail.com>
2025-03-26 16:40:38 +0700

    add google provider

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -2,3 +2,6 @@ export const HTTP_OK = 200;
 
 export const DEFAULT_PAGE_SIZE = 10;
 export const PAGE_SIZE_OPTION = ['10', '20', '50', '100'];
+
+export const GOOGLE_CLIENT_ID =
+  '917405119422-8o6u644otr6s9evm2m09h05cfubq8dq4.apps.googleusercontent.com';
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { GOOGLE_CLIENT_ID } from './constants/common.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </GoogleOAuthProvider>
 )
